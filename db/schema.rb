d42a413e9d50ecf7a9f9cb391bae51d7ea7fd658
Joel AZEMAR <joel.azemar@gmail.com>
2011-09-03 17:26:23 +0200

    Change wrong utf-8 declaration

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,68 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110903113111) do
+ActiveRecord::Schema.define(:version => 20110903133939) do
+
+  create_table "invitations", :force => true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "invitations", ["followed_id"], :name => "index_invitations_on_followed_id"
+  add_index "invitations", ["follower_id", "followed_id"], :name => "index_invitations_on_follower_id_and_followed_id", :unique => true
+  add_index "invitations", ["follower_id"], :name => "index_invitations_on_follower_id"
+
+  create_table "motivations", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notifications", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "sent_at"
+    t.text     "at_who"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "relationships", :force => true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], :name => "index_relationships_on_follower_id_and_followed_id", :unique => true
+  add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
+
+  create_table "token_types", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tokens", :force => true do |t|
+    t.integer  "token_type_id"
+    t.integer  "person_id"
+    t.decimal  "cost"
+    t.datetime "start_at"
+    t.datetime "stop_at"
+    t.boolean  "used"
+    t.integer  "motivation_id"
+    t.integer  "checkin_owner_id"
+    t.integer  "token_owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -24,10 +81,22 @@ ActiveRecord::Schema.define(:version => 20110903113111) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "authentication_token"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "gender"
+    t.string   "company"
+    t.string   "phone"
+    t.string   "twitter"
+    t.text     "bio"
+    t.boolean  "admin",                                 :default => false
+    t.string   "avatar"
+    t.string   "rpx_identifier"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
