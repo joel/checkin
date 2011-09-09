d1db67e253a90adf4c72f9994d2ae3611e40b3d3
Joel AZEMAR <joel.azemar@gmail.com>
2011-09-09 12:20:50 +0200

    Add Check Controller spec

--- spec/controllers/check_controller_spec.rb
+++ spec/controllers/check_controller_spec.rb
@@ -1,19 +1,30 @@
 require 'spec_helper'
 
 describe CheckController do
+  render_views
+  
+  before(:each) do
+    login_as_person_with_credits
+  end
+  
+  describe "GET new" do
+    it "assigns a new import as @import" do
+      get :new, :user_id => @user.id
+      assigns(:user).should eql(@user)
+    end
+  end
 
-  # describe "GET 'new'" do
-  #   it "should be successful" do
-  #     get 'new'
-  #     response.should be_success
-  #   end
-  # end
-
-  # describe "GET 'create'" do
-  #   it "should be successful" do
-  #     get 'create'
-  #     response.should be_success
-  #   end
-  # end
-
+  describe "POST create" do
+    describe "with valid params" do
+      it "create a new checkin" do
+        token = @user.tokens.available.first
+        token_type = token.token_type
+        motivation = token.motivation
+        post :create, :user_id => @user, :token_type_id => token_type, :motivation_id => motivation
+        assigns(:user).checkin?.should be_true
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+    end
+  end
+  
 end
