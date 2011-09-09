612726b969a0f8da416d066e7eba445ecbe2b86f
Joel AZEMAR <joel.azemar@gmail.com>
2011-09-09 11:30:55 +0200

    Add Motivation and RelationShip Specs

--- spec/models/motivation_spec.rb
+++ spec/models/motivation_spec.rb
@@ -1,5 +1,47 @@
 require 'spec_helper'
 
 describe Motivation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "associations" do
+    before { @invitation = Factory(:invitation) }
+    it "have a follow methods" do
+      [:followed, :follower, :accept!, :denied!].each do |method|
+        @invitation.should respond_to(method)
+      end
+    end
+  end
+  
+  describe "invitation : the followed" do
+    before do
+      @follower, @followed = Factory(:user), Factory(:user)
+      @followed.invitation!(@follower)
+      @invitation = @followed.invitations.first 
+    end
+    it "be able to accept an invitation" do
+      @invitation.accept!
+      @followed.invitations.size.should eql(0)
+      @follower.following?(@followed).should be_true
+    end
+    it "be able to denied an invitation" do
+      @invitation.denied!
+      @followed.invitations.size.should eql(0)
+      @followed.following?(@follower).should be_false
+    end
+  end
+  
+  describe "validations" do
+    before do
+      @follower, @followed = Factory(:user), Factory(:user)
+      @invitation = Factory.build(:invitation, :follower_id => nil, :followed_id => nil)
+    end
+    it "require a follower_id" do
+      @invitation.follower_id.should be_nil
+      @invitation.valid?.should be_false
+    end
+    it "require a followed_id" do
+      @invitation.followed_id.should be_nil
+      @invitation.valid?.should be_false
+    end
+  end
+
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
 describe Relationship do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "relationships" do
+    before do
+      @follower, @followed = Factory(:user), Factory(:user)
+      @relationship = @follower.relationships.build(:followed_id => @followed.id)
+    end
+    it "should create a new instance given valid attributes" do
+      expect { @relationship.save! }.to_not raise_error
+    end
+  end
+
+  describe "validations" do
+    before do
+      @follower, @followed = Factory(:user), Factory(:user)
+      @relationship = @follower.relationships.build(:followed_id => @followed.id)
+    end
+    it "require a follower_id" do
+      @relationship.follower_id = nil
+      @relationship.should_not be_valid
+    end
+    it "require a followed_id" do
+      @relationship.followed_id = nil
+      @relationship.should_not be_valid
+    end
+  end
+
+  describe "follow methods" do
+    before do
+      @follower, @followed = Factory(:user), Factory(:user)
+      @relationship = @follower.relationships.build(:followed_id => @followed.id)
+      @relationship.save!
+    end
+    it "have a follow methods" do
+      [:follower, :followed].each do |method|
+        @relationship.should respond_to(method)
+      end
+    end
+    it "have the right follower" do
+      @relationship.follower.should eq(@follower)
+    end
+    it "have the right followed user" do
+      @relationship.followed.should eq(@followed)
+    end
+  end
+
 end
