671368dad878a093b46d23079714b7e15cdb5303
Joel AZEMAR <joel.azemar@gmail.com>
2011-09-24 18:47:50 +0200

    Enhance configuration for Nginx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,16 @@ CheckinReloaded::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  # config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+  
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"  
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
