489f020f760de64abcf0617bce9981a1cb99f123
Joel AZEMAR <joel.azemar@gmail.com>
2011-10-22 17:55:38 +0200

    Enhance Rspec Config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,7 @@ Spork.prefork do
     # config.include ControllerMacros, :type => :helper
 
     config.use_transactional_fixtures = false
-    # config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.treat_symbols_as_metadata_keys_with_true_values = true
     config.filter_run :focus => true
     config.run_all_when_everything_filtered = true
 
