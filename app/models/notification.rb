d9680cf0ea0d19f5f7c488df6beace0eae7e4320
Joel AZEMAR <joel.azemar@gmail.com>
2011-10-28 18:11:35 +0200

    Add notification for all user, admin and member instead only member

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,27 +1,28 @@
 class Notification < ActiveRecord::Base
-  
+
   paginates_per 15
-  
+
   belongs_to :user
-  
+
   before_create :serialize_to
   after_create :send_email
-  
+
   serialize :at_who, Array
-  
-  private 
-  
+
+  private
+
   def send_email
     self.update_attribute(:sent_at, Time.now)
-    User.members.all.each do |member|
+    # User.members.all.each do |member|
+    User.all.each do |member|
       NotifierMailer.safe_sending { NotifierMailer.notify_people(self.user.id, self.id, member.id) }
     end
   end
-  
+
   def serialize_to
     a = []; User.members.all.each { |member| a << member.attributes }
     self.at_who = a
   end
-  
-  
+
+
 end
