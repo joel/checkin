124cf4586d4b1e8defe3a875bf37f9f068cd23cd
Joel AZEMAR <joel.azemar@gmail.com>
2012-03-25 17:09:16 +0200

    HOT FIX changes capistrano conf

--- config/deploy/resque.rb
+++ config/deploy/resque.rb
@@ -1,11 +1,9 @@
 namespace :resque do
   task :restart, :roles => :app do
-    sudo "monit restart resque_#{application}"
+    run "#{try_sudo} monit restart resque_#{application}"
   end
   
   task :start, :roles => :app do
-    sudo "monit start resque_#{application}"
+    run "#{try_sudo} monit start resque_#{application}"
   end
 end
-
-after 'deploy:symlink', 'resque:restart'
--- config/deploy/thin.rb
+++ config/deploy/thin.rb
@@ -1,14 +1,14 @@
 namespace :thin do
 
   task :restart, :roles => :app do
-    sudo "monit restart thin_#{application}-0"
+    run "#{try_sudo} monit restart thin_#{application}-0"
   end
 
   task :start, :roles => :app do
-    sudo "monit start thin_#{application}-0"
+    run "#{try_sudo} monit start thin_#{application}-0"
   end
 
   task :stop, :roles => :app do
-    sudo "monit stop thin_#{application}-0"
+    run "#{try_sudo} monit stop thin_#{application}-0"
   end
 end
