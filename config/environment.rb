b571c29fcb4caf1e19714e4c8d686793d3753c84
Joel AZEMAR <joel.azemar@gmail.com>
2012-04-12 17:02:01 +0200

    Add /usr/local/bin in Rails path for find identity needed by MiniMagick

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+# For MiniMagick https://forums.hostingplayground.com/showthread.php?p=3477
+ENV['PATH'] = "#{ENV['PATH']}:/usr/local/bin"
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 Encoding.default_external = "UTF-8"
